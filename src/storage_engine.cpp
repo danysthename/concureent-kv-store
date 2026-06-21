e2529d95dada00d9acb3491fa218dcadc9399d3e
Daniyaal Khan <khandaniyaal0@gmail.com>
2026-06-21 18:58:12 +0530

    completed implementation of the core storage engine

--- src/storage_engine.cpp
+++ src/storage_engine.cpp
@@ -1,13 +1,20 @@
 #include"storage_engine.h"
+#include<mutex>
+
 
 void storage_engine::PUT(std::string key, std::string value) {
-
+    std::unique_lock<std::shared_mutex> lock(mx);
+    mp[key] = value;
 }
 
 std::optional<std::string> storage_engine::GET(std::string key) {
-
+    std::shared_lock<std::shared_mutex> lock(mx);
+    if(mp.count(key))
+        return mp[key];
+    return {};
 }
 
 void storage_engine::DELETE(std::string key) {
-    
+    std::unique_lock<std::shared_mutex> lock(mx);
+    mp.erase(key);
 }
--- src/storage_engine.h
+++ src/storage_engine.h
@@ -1,8 +1,13 @@
 #pragma once
 #include <string>
-#include<optional>
+#include <optional>
+#include <unordered_map>
+#include<shared_mutex>
 
 class storage_engine {
+private:
+    std::unordered_map<std::string, std::string> mp;
+    std::shared_mutex mx;
 
 public:
     void PUT(std::string key, std::string value);
