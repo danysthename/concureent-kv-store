cb5d2039d0e38fa3bdf1171e9da6a68464f08eb0
Daniyaal Khan <khandaniyaal0@gmail.com>
2026-07-19 13:52:51 +0530

    built a working concurrent key-value store

---
+++ build/CMakeFiles/kv-store.dir/DependInfo.cmake
@@ -0,0 +1,25 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/daniyaalkhan/Documents/kv-store/src/main.cpp" "CMakeFiles/kv-store.dir/src/main.cpp.o" "gcc" "CMakeFiles/kv-store.dir/src/main.cpp.o.d"
+  "/home/daniyaalkhan/Documents/kv-store/src/storage_engine.cpp" "CMakeFiles/kv-store.dir/src/storage_engine.cpp.o" "gcc" "CMakeFiles/kv-store.dir/src/storage_engine.cpp.o.d"
+  "/home/daniyaalkhan/Documents/kv-store/src/wal.cpp" "CMakeFiles/kv-store.dir/src/wal.cpp.o" "gcc" "CMakeFiles/kv-store.dir/src/wal.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/kv-store.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/kv-store.dir/src/main.cpp.o"
+  "CMakeFiles/kv-store.dir/src/main.cpp.o.d"
+  "CMakeFiles/kv-store.dir/src/storage_engine.cpp.o"
+  "CMakeFiles/kv-store.dir/src/storage_engine.cpp.o.d"
+  "CMakeFiles/kv-store.dir/src/wal.cpp.o"
+  "CMakeFiles/kv-store.dir/src/wal.cpp.o.d"
+  "kv-store"
+  "kv-store.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/kv-store.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
